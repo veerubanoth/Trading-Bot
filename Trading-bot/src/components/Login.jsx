b921dedb673cdb3833b4a897669d9f33c2304b96
veerubanoth <veerubanoth@gmail.com>
2023-08-17 12:46:59 +0530

    trading-bot

--- Trading-bot/src/components/Login.jsx
+++ Trading-bot/src/components/Login.jsx
@@ -1,24 +1,27 @@
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 export const Login = () => {
   const form = useForm();
+  const { register } = form;
 
   return (
     <div>
       <form>
         <label htmlFor="Fullname">Full name</label>
-        <input type="text" id="Fullname" name="Fullname" />
+        <input type="text" id="Fullname" {...register("Fullname")} />
 
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username" {...register("username")} />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
+        <input type="password" id="password" {...register("password")} />
 
         <button>Create Account</button>
+        <DevTool />
       </form>
     </div>
   );
